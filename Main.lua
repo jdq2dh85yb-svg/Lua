4460cfe3c6b7b847398c1c10d220011373f6b00c
jdq2dh85yb-svg <jdq2dh85yb@privaterelay.appleid.com>
2026-04-15 07:52:15 +0200

    Main.lua aktualisieren

--- Main.lua
+++ Main.lua
@@ -35,7 +35,7 @@ local S = {
 -- kostenlosen Hosting-Dienst hochladen z.B.:
 -- https://pages.github.com  oder  https://netlify.com
 ------------------------------------------------
-local KEY_WEBSITE = "https://DEINE-URL-HIER.com"
+local KEY_WEBSITE = "https://tiiny.host/manage/preview"
 
 ------------------------------------------------
 -- KEY VALIDIERUNG
